ba5589a0f2096fdd39ce08a8d02951bd3e62e319
den.lysenko <den.l@april33.com>
2018-06-04 00:17:06 +0300

    NS: dependencies for core module

--- apps/mobile/app/app.module.ts
+++ apps/mobile/app/app.module.ts
@@ -1,17 +1,11 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
-import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing';
 import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
   imports: [
-    NativeScriptModule,
-    NativeScriptHttpClientModule,
-    CoreModule,
+    CoreModule
     // AppRoutingModule
   ],
   declarations: [AppComponent],
--- apps/mobile/app/modules/core/core.module.ts
+++ apps/mobile/app/modules/core/core.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 
+import { RouterExtensions, StoragePlatformService } from '@bookapp-angular/core';
+import { GraphQLModule } from '@bookapp-angular/graphql';
+import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { RouterExtensions as TNSRouterExtensions } from 'nativescript-angular/router';
 import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
+
+import { StorageService } from './services/storage.service';
 
 @NgModule({
   imports: [
+    NativeScriptModule,
+    NativeScriptHttpClientModule,
     TNSFontIconModule.forRoot({
-      fa: './assets/font-awesome.min.css'
-    })
+      mdi: 'material-design-icons.css'
+    }),
+    GraphQLModule.forRoot()
   ],
-  providers: []
+  providers: [
+    {
+      provide: StoragePlatformService,
+      useClass: StorageService
+    },
+    {
+      provide: RouterExtensions,
+      useClass: TNSRouterExtensions
+    }
+  ]
 })
 export class CoreModule {}
