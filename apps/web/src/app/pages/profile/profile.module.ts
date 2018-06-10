defaea1b67e4e715f01adafff13412963109fd9f
den.lysenko <den.l@april33.com>
2018-06-10 20:31:37 +0300

    Web: ProfileForm layout

--- apps/web/src/app/pages/profile/profile.module.ts
+++ apps/web/src/app/pages/profile/profile.module.ts
@@ -1,15 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule
+} from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { RouterModule } from '@angular/router';
 
+import { components } from './components';
+import { containers } from './containers';
 import { routes } from './profile.routes';
 
-import { containers } from './containers';
-import { components } from './components';
-
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   declarations: [...containers, ...components]
 })
 export class ProfileModule {}
