5abbdfd40c3541f699fe3ee24a5982a3a4e59a1e
den.lysenko <den.l@april33.com>
2018-07-01 12:02:14 +0300

    Web: BookFilter logic moved to abstract class

--- apps/web/src/app/modules/books/components/book-filter/book-filter.component.ts
+++ apps/web/src/app/modules/books/components/book-filter/book-filter.component.ts
@@ -1,11 +1,6 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatButtonToggleChange } from '@angular/material';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
-
-import { BookFilter } from '@bookapp-angular/books-core';
-import { BaseComponent } from '@bookapp-angular/core';
+import { BookFilterBaseComponent } from '@bookapp-angular/books-core';
 
 @Component({
   selector: 'ba-book-filter',
@@ -13,31 +8,8 @@ import { BaseComponent } from '@bookapp-angular/core';
   styleUrls: ['./book-filter.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookFilterComponent extends BaseComponent implements OnInit {
-  searchQuery = new FormControl();
-  sortValue = '';
-
-  @Input()
-  set filter(value: BookFilter) {
-    if (value && value.searchQuery) {
-      this.searchQuery.setValue(value.searchQuery, { emitEvent: false });
-    }
-
-    if (value && value.sortValue) {
-      this.sortValue = value.sortValue;
-    }
-  }
-
-  @Output() onSort = new EventEmitter<string>();
-  @Output() onSearch = new EventEmitter<string>();
-
-  ngOnInit() {
-    this.searchQuery.valueChanges
-      .pipe(debounceTime(500), distinctUntilChanged(), takeUntil(this.destroy$))
-      .subscribe(val => this.onSearch.emit(val));
-  }
-
-  sort(e: MatButtonToggleChange) {
-    this.onSort.emit(e.value);
+export class BookFilterComponent extends BookFilterBaseComponent {
+  constructor() {
+    super();
   }
 }
