7799497903643be36ce4219a7a31f42d90069f94
den.lysenko <den.l@april33.com>
2018-07-24 13:26:34 +0300

    Web: display button depends on bookmarks

--- apps/web/src/app/modules/books/components/book-view/book-view.component.ts
+++ apps/web/src/app/modules/books/components/book-view/book-view.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { User } from '@bookapp-angular/auth-core';
 import { Book } from '@bookapp-angular/books-core';
+import { BOOKMARKS } from '@bookapp-angular/core';
 
 @Component({
   selector: 'ba-book-view',
@@ -21,6 +22,18 @@ export class BookViewComponent {
 
   get isAdmin(): boolean {
     return this._isAdmin;
+  }
+
+  get inFavourites(): boolean {
+    return this.bookmarks && this.bookmarks.includes(BOOKMARKS.FAVOURITE);
+  }
+
+  get inWishlist(): boolean {
+    return this.bookmarks && this.bookmarks.includes(BOOKMARKS.WISHLIST);
+  }
+
+  get inMustread(): boolean {
+    return this.bookmarks && this.bookmarks.includes(BOOKMARKS.MUSTREAD);
   }
 
   private _isAdmin: boolean;
--- libs/core/src/lib/constants.ts
+++ libs/core/src/lib/constants.ts
@@ -1,8 +1,18 @@
 export const AUTH_TOKEN = 'ba_auth_token';
+
 export const ADMIN_ROLE = 'admin';
+
 export const FILTER_KEYS = {
   BROWSE_BOOKS: 'BROWSE_BOOKS',
   BUY_BOOKS: 'BUY_BOOKS'
 };
+
 export const LIMIT = 10;
+
 export const DEFAULT_SORT_VALUE = 'id_desc';
+
+export const BOOKMARKS = {
+  MUSTREAD: 'MUSTREAD',
+  WISHLIST: 'WISHLIST',
+  FAVOURITE: 'FAVOURITE'
+};
