50fa5cbf8c8452d2bf9ac8064fa108cd46d46e8f
den.lysenko <den.l@april33.com>
2018-09-09 12:52:43 +0300

    Mobile: app shortcuts

--- apps/mobile/app/app.component.ts
+++ apps/mobile/app/app.component.ts
@@ -1,7 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { AppShortcuts } from 'nativescript-app-shortcuts';
+import { RouterExtensions } from '@bookapp-angular/core';
 
 @Component({
   selector: 'ba-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private routerExtensions: RouterExtensions,
+    private zone: NgZone
+  ) {
+    new AppShortcuts().setQuickActionCallback(shortcutItem => {
+      if (shortcutItem.type === 'reading') {
+        return this.deeplink('/books/read');
+      }
+
+      if (shortcutItem.type === 'browse') {
+        return this.deeplink('/books/browse');
+      }
+
+      if (shortcutItem.type === 'buy') {
+        return this.deeplink('/books/buy');
+      }
+    });
+  }
+
+  private deeplink(to: string) {
+    this.zone.run(() => {
+      this.routerExtensions.navigate([to], {
+        animated: false
+      });
+    });
+  }
+}
--- apps/mobile/app/app.module.ts
+++ apps/mobile/app/app.module.ts
@@ -20,7 +20,4 @@ import { AppRoutingModule } from './app.routing';
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
-/*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
 export class AppModule {}
